2e65a52c98e76e3877490ce4d0a1468fbe052875
Patrick Byers <byerspatr@stellar.ecs.vuw.ac.nz>
2015-07-29 15:38:44 +1200

    Updated the player to stop when a sequence is complete

--- MekBassMidiMaker/src/tools/Player.java
+++ MekBassMidiMaker/src/tools/Player.java
@@ -8,6 +8,8 @@ import javax.sound.midi.*;
 public class Player {
 
 	private Sequencer seq;
+	private long startTime;
+	private long endTime;
 
 	public Player() {
 		try {
@@ -22,21 +24,51 @@ public class Player {
 		} catch (MidiUnavailableException e) {
 			System.err.println("Sequencer not avaliable");
 		}
+		seq.setLoopCount(1);
 	}
 
+//	public void play(Sequence s) {
+//		try {
+//			seq.setSequence(s);
+//		} catch (InvalidMidiDataException e) {
+//			System.err.println("Invalid midi data encountered");
+//		}
+//		setPoints(s);
+//		System.out.println("Starting playback");
+//		seq.start();
+//		System.out.println(startTime+" : "+endTime+" Giving a play time of "+(endTime-startTime));
+//		try {
+//			Thread.sleep((endTime-startTime)/1000 + 1);
+//		} catch (InterruptedException e) {
+//			System.err.println("Thread Interupted error");
+//		}
+//		seq.stop();
+//		System.out.println("Stopped");
+//	}
+//
 	public void play(Sequence s) {
 		try {
 			seq.setSequence(s);
 		} catch (InvalidMidiDataException e) {
 			System.err.println("Invalid midi data encountered");
 		}
-		System.out.println("Starting playback"); 
+		setPoints(s);
+		System.out.println("Starting playback");
+		seq.setLoopStartPoint(startTime);
+		seq.setLoopEndPoint(endTime);
 		seq.start();
+		System.out.println(startTime+" : "+endTime+" Giving a play time of "+(endTime-startTime));
+	}
+
+
+	private void setPoints(Sequence s){
+		startTime = Timing.getMinTime(s);
+		endTime = Timing.getMaxTime(s);
 	}
 
 	public static void main(String[] args) {
 		Player p = new Player();
-		File midFile = new File("resources/stairway.mid");
+		File midFile = new File("resources/Twinkle_Twinkle_in_octaves.mid");
 		try {
 			Sequence s = MidiSystem.getSequence(midFile);
 			p.play(s);
