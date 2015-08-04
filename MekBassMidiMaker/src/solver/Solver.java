e955ed5311d598a29d91c8b14bc4f1a3296c795d
Elliot Wilde <wildeelli@myvuw.ac.nz>
2015-08-05 10:36:38 +1200

    Basic Solver Complete

--- MekBassMidiMaker/src/solver/Solver.java
+++ MekBassMidiMaker/src/solver/Solver.java
@@ -18,6 +18,7 @@ public class Solver {
 																new MekString(33, 46), new MekString(28, 41)};
 	private static long[] stringTimes;
 	private static int[] lastNote;
+	private static int lastString = -1;
 
 	static Sequence solve(Sequence seq){
 		// array of long for strings representing timestamp of most recent finished note
@@ -40,29 +41,61 @@ public class Solver {
 					if (shrtmsg.getData2()!=0){
 						// find the right string
 						int note = shrtmsg.getData1();
-						List<MekString> rightStrings = new ArrayList<MekString>();
-						for (MekString ms: strings){
-							if (note > ms.lowNote && note < ms.highNote) {
-								rightStrings.add(ms);
+						List<Integer> rightStrings = new ArrayList<Integer>();
+						for (int j = 0; j < strings.length; j++){
+							if (note > strings[j].lowNote && note < strings[j].highNote && lastNote[j] == -1) {
+								rightStrings.add(j);
 							}
 						}
-						// we now have a list of strings which can theoretically play the note, one or more may be occupied
-
-
-						// put it there
-
-						// put it in last note for that string
+						// we now have a list of strings which can theoretically play the note. we assign it to
+						//the string that was played the longest time ago.
+						//TODO: figure out which is closer to the middle
+						long minTime = Long.MAX_VALUE;
+						int useString = -1;
+						for(Integer j : rightStrings){
+							if (stringTimes[j] < minTime){
+								minTime = stringTimes[j];
+								useString = j;
+							}
+						}
+						if(useString >= 0 && useString < strings.length){
+							// put it there if it is valid
+							moveEvent(tr,seq.getTracks()[useString+1],tr.get(i));
+							// put it in last note for that string
+							lastNote[useString] = note;
+							lastString = useString;
+						}
 						break;
 					}
 					// if the if was false, then it is actually a note off
 				case NOTE_OFF:
+					int note = shrtmsg.getData1();
+					int useString = -1;
+					for (int j=0; j < strings.length; ++j){
+						if (lastNote[j]==note) useString=j;
+					}
+					if (useString==-1) break; // if the note wasn't played, dont do anything with it
+					// now we have the correct string
+					// so we add the noteoff to the correct track
+					moveEvent(tr,seq.getTracks()[useString+1],tr.get(i));
+
+					// and set the other stuff to nothing
+					lastNote[useString] = -1;
+					stringTimes[useString] = tr.get(i).getTick();
+					lastString = -1;
 					break;
+				case PITCH_BEND:
 					default:
+					// if there is a string playing
+					if (lastString >= 0) {
+						// add it to that
+						moveEvent(tr,seq.getTracks()[lastString+1],tr.get(i));
+					}
 				}
 			}
 		}
 
-		return null;
+		return seq;
 	}
 
 	/**
