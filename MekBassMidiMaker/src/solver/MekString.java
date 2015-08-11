f632bd3d153ccfe14d5b98e1dc3af8615dbe77c9
Elliot Wilde <wildeelli@myvuw.ac.nz>
2015-08-12 11:34:14 +1200

    Added comments to the public fields in MekString, also revised class docs

--- MekBassMidiMaker/src/solver/MekString.java
+++ MekBassMidiMaker/src/solver/MekString.java
@@ -1,28 +1,46 @@
 package solver;
 
 /**
- * tuple for string information?<br>
- *		note top/bottom<br>
- *		time between frets<br>
- *		channel/track?
+ * Tuple for string information<p>
+ * Contains: <br>
+ * Highest and Lowest notes that the string can play. <br>
+ * The Range of Notes that the string can play. <br>
+ * The time taken to travel between two adjacent notes.
+ * @author Elliot Wilde, Andrew Palmer
  */
 public class MekString {
+	/**
+	 * The Lowest note in MIDI notation that the string can play;
+	 */
 	public final int lowNote;
+	/**
+	 * The Highest note in MIDI notation that the string can play;
+	 */
 	public final int highNote;
-	public final int[] interval; // made an array as they are different between different frets 
+	/**
+	 * The Highest note minus the Lowest note in MIDI notation that the string can play;
+	 */
+	public final int noteRange;
+	/**
+	 * The Interval (in μs) between adjacent frets.<br>
+	 * The time between the first and second frets would be in index 0
+	 */
+	public final int[] interval;
 
 	public MekString(int low, int high, int[] time){
 		this.lowNote = low;
 		this.highNote = high;
 		this.interval = time;
+		this.noteRange = highNote - lowNote;
 	}
 
 	public MekString(int low, int high){
 		this.lowNote = low;
 		this.highNote = high;
-		this.interval = new int[]{0};
+		this.noteRange = highNote - lowNote;
+		this.interval = new int[this.noteRange];
 	}
-	
+
 	/**
 	 * returns the sum of all intervals between start and stop.
 	 * @param start
@@ -30,18 +48,18 @@ public class MekString {
 	 * @return Sum of intervals
 	 */
 	public int addIntervals(int start, int stop){
-		int ret = 0; 
+		int ret = 0;
 		for(int i = start; i < stop; i++){ //not inclusive as start and stop represent notes and interval is between them
 			ret += interval[i];
 		}
 		return ret;
 	}
-	
+
 	/**
 	 * Checks if two notes on the string are conflicting
 	 * @param note1
 	 * @param note2
-	 * @return 
+	 * @return
 	 */
 	public boolean conflicting(int note1,int note2,int duration){
 		if(addIntervals(note1-lowNote,note2-lowNote)< duration) return true;
