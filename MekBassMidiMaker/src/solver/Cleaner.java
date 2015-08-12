b4f01804522fe8ba686813eba9cade19e91a5911
Andrew Palmer <andrewpalmer.me@gmail.com>
2015-08-12 14:58:16 +1200

    prepositioning added

--- MekBassMidiMaker/src/solver/Cleaner.java
+++ MekBassMidiMaker/src/solver/Cleaner.java
@@ -83,19 +83,15 @@ public class Cleaner {
 					// check what kind of command it is (note on/off, etc)
 					if(noteOn.getCommand() == NOTE_ON){
 						// check whether or not it is actually a note on...
-						if (noteOn.getData2()!=0 && noteOn.getData2()!=1){
+						if (noteOn.getData2()!=1){
 							int note1 = noteOn.getData1();
 							//find the previous note off
-							MidiMessage midNoteOff;
-							for(int k = j-1; k == 0; --k){
-								midNoteOff = cur.get(k).getMessage();
-								if(midNoteOff instanceof ShortMessage){
-									ShortMessage noteOff = (ShortMessage) midNoteOff;
-									if(noteOff.getCommand() == NOTE_OFF){
-										int note2 = noteOff.getData1();
-										if(strings[i].conflicting(note1, note2, cur.get(k).getTick() - cur.get(j).getTick())) conflicts++;
-									}
-								}
+							ShortMessage noteOff;
+							int prevIndex = getPrev(j,cur);
+							if(prevIndex>0){
+								noteOff = (ShortMessage) cur.get(prevIndex).getMessage();
+								int note2 = noteOff.getData1();
+								if(strings[i].conflicting(note1, note2, cur.get(prevIndex).getTick() - cur.get(j).getTick())) conflicts++;
 							}
 						}
 					}
@@ -104,14 +100,61 @@ public class Cleaner {
 		}
 		return conflicts;
 	}
+	
+	/**
+	 * Gets the previous note 
+	 */
+	private static int getPrev(int index, Track cur){ 
+		for(int k = index-1; k == 0; --k){
+			MidiMessage midNoteOff = cur.get(k).getMessage();
+			if(midNoteOff instanceof ShortMessage){
+				ShortMessage noteOff = (ShortMessage) midNoteOff;
+				if(noteOff.getCommand() == NOTE_OFF){
+					return k;
+				}
+			}
+		}
+		return 0;
+	}
 
 	/**
 	 * Adds pre positioning notes to the MIDI so MekBass can play it
-	 * @param seq
+	 * @param seq - The sequence to add prepositioning to.
+	 * @param preTime - The time in É s to add before the string MUST be prepositioned. 
 	 * @return
 	 */
-	public static Sequence prePos(Sequence seq){
-		//TODO: implement
+	public static Sequence prePos(Sequence seq, long preTime, MekString[] strings){
+		//for each track
+		for(int i = 0; i < seq.getTracks().length; i++){
+			Track cur = seq.getTracks()[i];
+			//add a prepos event for each note that is not consecutive
+			for(int j = 0; j < cur.size(); j++){
+				MidiMessage midNoteOn = cur.get(j).getMessage();
+				if (midNoteOn instanceof ShortMessage){
+					ShortMessage noteOn = (ShortMessage) midNoteOn;
+					if(noteOn.getCommand() == NOTE_ON){
+						// check whether or not it is actually a note on...
+						if (noteOn.getData2()!=1){
+							int note1 = noteOn.getData1();
+							//find the previous note off
+							ShortMessage noteOff;
+							int prevIndex = getPrev(j,cur);
+							if(prevIndex>0){
+								noteOff = (ShortMessage) cur.get(prevIndex).getMessage();
+								int note2 = noteOff.getData1();
+								//if the note is different and they don't clash
+								if(note2 != note1 && !strings[i].conflicting(note1, note2, cur.get(prevIndex).getTick() - cur.get(j).getTick() - preTime)){
+									cur.add(new MidiEvent(new ShortMessage(),cur.get(j).getTick() - strings[i].difference(note1, note2) - preTime));
+								}
+								else{
+									System.out.printf("%d: note %d to close to preceeding note.", cur.get(j).getTick() ,note1);
+								}
+								}
+						}
+					}
+				}
+			}
+		}
 		return seq;
 	}
 }
