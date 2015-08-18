20ea11e914a79d2557e94f3c3c75031efb72165e
Andrew Palmer <andrewpalmer.me@gmail.com>
2015-08-19 11:32:03 +1200

    fixed conflict checking

--- MekBassMidiMaker/src/solver/MekString.java
+++ MekBassMidiMaker/src/solver/MekString.java
@@ -63,7 +63,7 @@ public class MekString {
 	 * @return the 
 	 */
 	public boolean conflicting(int note1,int note2, long duration){
-		if(addIntervals(note1-lowNote,note2-lowNote)< duration) return true;
+		if(addIntervals(note1-lowNote,note2-lowNote) > duration) return true;
 		return false;
 	}
 	
