38d645004a44e74a242fa78d66b60d368de41e53
Patrick Byers <byerspatr@stellar.ecs.vuw.ac.nz>
2015-09-16 15:51:42 +1200

    update to console

--- MekBassMidiMaker/src/ui/Console.java
+++ MekBassMidiMaker/src/ui/Console.java
@@ -1,11 +1,14 @@
 package ui;
 
 import helperCode.OctaveShifter;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.Stack;
+
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequence;
@@ -16,8 +19,9 @@ import solver.TrackSplitter;
 import tools.Player;
 
 /**
- * The console used in both the GUI and non-GUI modes, it is responsible for receiving user's text input
- * and displaying any text output.
+ * The console used in both the GUI and non-GUI modes, it is responsible for
+ * receiving user's text input and displaying any text output.
+ *
  * @author Patrick Byers
  *
  */
@@ -28,20 +32,31 @@ public class Console extends OutputStream {
 	BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
 	String input = "i";
 	Slave slave;
-/**
- * Create a console using for use within the gui.
- * @param text The text of the GUI for this console to use
- * @param slave The slave instance
- */
+	Stack<String> prevCommand = new Stack<String>();
+	Stack<String> commandStack = new Stack<String>();
+	String SelectedCommand;
+	String allText;
+
+	/**
+	 * Create a console using for use within the gui.
+	 *
+	 * @param text
+	 *            The text of the GUI for this console to use
+	 * @param slave
+	 *            The slave instance
+	 */
 	public Console(TextArea text, Slave slave) {
 		guiMode = true;
 		area = text;
 		this.slave = slave;
 	}
-/**
- * Create a console for use without a GUI.
- * @param slave The slave instance.
- */
+
+	/**
+	 * Create a console for use without a GUI.
+	 *
+	 * @param slave
+	 *            The slave instance.
+	 */
 	public Console(Slave slave) {
 		guiMode = false;
 		this.slave = slave;
@@ -61,26 +76,36 @@ public class Console extends OutputStream {
 			}
 		} while (!input.equals("END"));
 	}
-/**
- * Parse a block of input
- * @param text The block of input
- */
+
+	/**
+	 * Parse a block of input
+	 *
+	 * @param text
+	 *            The block of input
+	 */
 	protected void Parse(String text) {
+		if(text.equals("") || text == null || text.equals("\n")){
+			area.setText("No command input");
+			area.appendText("");
+			return;
+		}
 		String[] lines = text.split("\n");
-		String rawInput = lines[lines.length-1].trim();
+		String rawInput = lines[lines.length - 1].trim();
 		String[] input = rawInput.split("\\s+");
 		String command = input[0];
+		commandStack.push(rawInput);
 		switch (command) {
 		case "open":
-			if(input.length>1)
-			open(rawInput);
-			else output("no file specified");
+			if (input.length > 1)
+				open(rawInput);
+			else
+				output("no file specified");
 			break;
 		case "solve":
-			if(input.length>1){
+			if (input.length > 1) {
 				this.solve(rawInput);
-			}
-			else slave.solve();
+			} else
+				slave.solve();
 			break;
 		case "play":
 			slave.play();
@@ -95,57 +120,82 @@ public class Console extends OutputStream {
 			slave.octaveDown();
 			break;
 		case "save":
-			if (input.length>1){
+			if (input.length > 1) {
 				this.save(rawInput);
-			}
-			else this.save();
+			} else
+				this.save();
 			break;
 		case "END":
 			System.exit(0);
 		default:
 			output("Command not recongnized");
 		}
+		prevCommand = commandStack;
+		allText = area.getText();
 	}
-/**
- * Returns whether or not the console is set to GUI mode
- * @return GUI mode
- */
+
+	/**
+	 * Returns whether or not the console is set to GUI mode
+	 *
+	 * @return GUI mode
+	 */
 	protected boolean getGUIMode() {
 		return guiMode;
 	}
 
-	protected void open(String input){
+	protected void prevCommand(){
+		area.clear();
+		this.SelectedCommand = prevCommand.pop();
+		output(allText + SelectedCommand);
+	}
+
+	protected void open(String input) {
 		String FileName = input.substring(4).replace("\"", "").trim();
 		Slave.setCurMIDI(FileName);
 	}
 
-	protected void save(String input){
+	protected void save(String input) {
 		String fileName = input.substring(4).replace("\"", "").trim();
 		Slave.save(fileName);
 	}
 
-	protected void save(){
+	protected void save() {
 		output("saving file as \"out.mid\" in the current directory");
 		Slave.save("out.mid");
 	}
 
-	protected void solve(String input){
+	protected void solve(String input) {
 		String FileName = input.substring(5).replace("\"", "").trim();
-		if(Slave.setCurMIDI(FileName)){
+		if (Slave.setCurMIDI(FileName)) {
 			slave.solve();
 		}
 	}
+
 	/**
 	 * Output the string to the appropriate area
+	 *
 	 * @param text
 	 */
 	protected void output(String text) {
-		System.out.print(text + "\n");
+		for (char c: text.toCharArray()){
+			try {
+				write((int) c);
+			} catch (IOException e) {
+				System.out.write((int) c);
+			}
+		}
+		try {
+			write('\n');
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.write('\n');
+		}
 	}
 
 	@Override
 	public void write(int i) throws IOException {
-		area.appendText(String.valueOf((char) i));
+		if(guiMode) area.appendText(String.valueOf((char) i));
+		else System.out.write(i);
 
 	}
 }
