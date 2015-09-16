3d63a21d8e410748ae8d80dd380670029da13260
Andrew Palmer <andrewpalmer.me@gmail.com>
2015-09-16 15:59:15 +1200

    split&solve now st up to give hysteria output.

--- MekBassMidiMaker/src/solver/SplitAndSolve.java
+++ MekBassMidiMaker/src/solver/SplitAndSolve.java
@@ -42,9 +42,10 @@ public class SplitAndSolve {
 
 
 			try {
-				MidiSystem.write(out, 1, new File("out.mid"));
+				out = Cleaner.clean(out);
+				MidiSystem.write(out, 1, new File("hyst.mid"));
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
+				System.out.println("Write Failed\n");
 				e.printStackTrace();
 			}
 
@@ -68,15 +69,17 @@ public class SplitAndSolve {
 			sequence = sequencer.getSequence();
 		}
 		catch (MidiUnavailableException e) {
+			System.out.println("Midi Unavailable\n");
 			e.printStackTrace();
 		}
 		catch (InvalidMidiDataException e){
+			System.out.println("Midi Invalid\n");
 			e.printStackTrace();
 		}
 		catch (IOException e) {
+			System.out.println("IO Exception\n");
 			e.printStackTrace();
 		}
-		SplitAndSolve sAndS =  new SplitAndSolve(sequence,4,1);
-
+		SplitAndSolve sAndS =  new SplitAndSolve(sequence,4,9);
 	}
 }
