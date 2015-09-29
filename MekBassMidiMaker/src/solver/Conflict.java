c3da7bf5861c4124e108c541fab75dd798d2a4d1
Andrew Palmer <andrewpalmer.me@gmail.com>
2015-09-30 12:11:28 +1300

    made conflict grabbing easier

--- MekBassMidiMaker/src/solver/Conflict.java
+++ MekBassMidiMaker/src/solver/Conflict.java
@@ -1,7 +1,10 @@
 package solver;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
 import javax.sound.midi.MidiEvent;
 
 /**
@@ -11,15 +14,15 @@ import javax.sound.midi.MidiEvent;
  */
 public class Conflict {
 	private MidiEvent note;
-	private Map<Integer,NoteConflict> conflicts;
+	private List<NoteConflict> conflicts;
 	private boolean resolved;
 	
 	public Conflict(MidiEvent n){
-		note = n; conflicts = new HashMap<Integer,NoteConflict>(); resolved = false;
+		note = n; conflicts = new ArrayList<NoteConflict>(); resolved = false;
 	}
 	
-	public void addConf(NoteConflict con, int string){
-		conflicts.put(string,con);
+	public void addConf(NoteConflict con){
+		conflicts.add(con);
 	}
 	
 	public boolean resolved(){
@@ -30,12 +33,32 @@ public class Conflict {
 		return note;
 	}
 	
-	public NoteConflict getConf(int str){
-		return conflicts.get(str);
+	public List<NoteConflict> getConf(){
+		return conflicts;
 	}
 	
 	public int strings(){
 		return conflicts.size();
+	}
+	
+	public long start(){
+		long min = Long.MAX_VALUE;
+		for(NoteConflict con : conflicts){
+			if(con.tick() < min){
+				min = con.tick();
+			}
+		}
+		return min;
+	}
+	
+	public long end(){
+		long max = 0;
+		for(NoteConflict con : conflicts){
+			if(con.maxTick() > max){
+				max = con.tick();
+			}
+		}
+		return max;
 	}
 	
 	public String toString(){
