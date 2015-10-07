cd03c0f89b705d8a90fe0f940d335c1c0e70eadd
Andrew Palmer <andrewpalmer.me@gmail.com>
2015-10-07 14:39:33 +1300

    some test changes

--- MekBassMidiMaker/src/solver/tests/CleanerTest.java
+++ MekBassMidiMaker/src/solver/tests/CleanerTest.java
@@ -283,7 +283,7 @@ public class CleanerTest {
 			compare.getTracks()[1].add(CleanerTest.makeNoteOff(2,234,1));
 			long f = (long) ((float)seq.getMicrosecondLength()/(float)seq.getTickLength());
 			Cleaner.prePos(seq,100,new MekString[]{new MekString(1,2,new long[]{1000})},14);
-			System.out.printf("Track Length:\n uS = %d \n ticks: %d \n uS/ticks: %d\n",seq.getMicrosecondLength(),seq.getTickLength(),f);
+			//System.out.printf("Track Length:\n uS = %d \n ticks: %d \n uS/ticks: %d\n",seq.getMicrosecondLength(),seq.getTickLength(),f);
 			assertTrue(seq.equals(compare));
 		} catch (InvalidMidiDataException e) {
 			fail("this shouldn't happen");
