174abc18b183e304781dd686347328cf6c86d69c
Andrew Palmer <andrewpalmer.me@gmail.com>
2015-10-07 14:39:10 +1300

    mekstring should be fine now

--- MekBassMidiMaker/src/solver/MekString.java
+++ MekBassMidiMaker/src/solver/MekString.java
@@ -106,7 +106,7 @@ public class MekString {
 	
 	public long differenceTick(int note1, int note2, float scale){
 //		System.out.printf("Difference in ticks: %d\n time: %d\n scale = %d\n", thing, differenceTime(note1,note2),(long)scale);
-		return (long) (differenceTime(note1,note2)/scale);
+		return (long) (differenceTime(note1,note2)*scale);
 	}
 	
 	public boolean playable(int i){
